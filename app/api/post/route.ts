b677aebb2cd796968cab3f007b4dc6ba1bc627cd
hyeyeon345 <22011769@sju.ac.kr>
2025-06-16 12:27:30 +0900

    add route

---
+++ app/api/post/route.ts
@@ -0,0 +1,10 @@
+import prisma from '@/lib/server/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const postList = await prisma.post.findMany({})
+    return NextResponse.json({
+        postList,
+        ok:true,
+    })
+}
